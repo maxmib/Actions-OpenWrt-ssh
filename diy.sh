1de0ee01eaf49e76bfd7aab5a895329451855f9e
maxmib <30944505+maxmib@users.noreply.github.com>
2020-04-24 12:49:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,3 +10,4 @@
 #cd package/lean  
 #rm -rf luci-theme-argon  
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+sed -i 's/#src-git/src-git/g' feeds.conf.default && ./scripts/feeds clean && ./scripts/feeds update -a && ./scripts/feeds install -a
