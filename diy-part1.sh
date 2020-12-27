b7953cd3e9844c84cf0ccb7ae3cdb93eca9f794c
maxmib <30944505+maxmib@users.noreply.github.com>
2020-12-27 22:54:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
  Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/clark15b/xupnpd2.git package/xupnpd2
+
