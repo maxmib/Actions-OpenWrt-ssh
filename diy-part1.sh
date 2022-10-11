c866aab74ce7a7fddbd551f7cf54bf0845f88483
maxmib <30944505+maxmib@users.noreply.github.com>
2022-10-11 19:40:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,10 +8,10 @@
 # Blog: https://p3terx.com
 #=============================================================
 
- Uncomment a feed source
+# Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
- Add a feed source
+# Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
