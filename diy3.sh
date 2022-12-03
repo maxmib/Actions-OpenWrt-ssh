19642dcf9a01f4cf3b1179ae9d7b08070b74cfaa
maxmib <30944505+maxmib@users.noreply.github.com>
2022-12-03 21:09:29 +0800

    Update diy3.sh

--- diy3.sh
+++ diy3.sh
@@ -13,12 +13,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git small2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git small2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
+git clone https://github.com/honwen/luci-app-aliddns.git  package/luci-app-aliddns
 #rm -rf package/lean/luci-theme-argon
