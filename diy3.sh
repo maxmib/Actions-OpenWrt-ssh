e55758988a277818d3adaa72ebeda4c3b43b96d2
maxmib <30944505+maxmib@users.noreply.github.com>
2022-12-03 13:08:22 +0800

    Update diy3.sh

--- diy3.sh
+++ diy3.sh
@@ -13,7 +13,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
