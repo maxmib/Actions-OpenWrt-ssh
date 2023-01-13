06541e082c6e3590de3f15d2d541acca8fa98daf
maxmib <30944505+maxmib@users.noreply.github.com>
2023-01-14 02:40:35 +0800

    Update diy4.sh

--- diy4.sh
+++ diy4.sh
@@ -11,3 +11,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8/5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72/g' feeds/packages/utils/jq/Makefile
+sed -i 's/result.insecure = "0"/result.insecure = "1"/g' package/feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
