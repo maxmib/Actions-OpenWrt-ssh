b17cc80de7e4e3ba838f4b4744f741d06ef8fda0
maxmib <30944505+maxmib@users.noreply.github.com>
2023-01-14 14:36:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/result.insecure = "0"/result.insecure = "1"/g' package/feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
