30d62c9d9d18725af38bddefffb8ae03f908f100
maxmib <30944505+maxmib@users.noreply.github.com>
2025-07-05 13:00:52 +0800

    Update diy4.sh

--- diy4.sh
+++ diy4.sh
@@ -12,3 +12,11 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/9625784cf2e4fd9842f1d407681ce4878b5b0dcddbcd31c6135114a30c71e6a8/5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72/g' feeds/packages/utils/jq/Makefile
 sed -i 's/result.insecure = "0"/result.insecure = "1"/g' package/feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+
+# Set Rust build arg llvm.download-ci-llvm to false.
+RUST_MAKEFILE="feeds/packages/lang/rust/Makefile"
+if [[ -f "${RUST_MAKEFILE}" ]]; then
+  sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' "${RUST_MAKEFILE}"
+else
+  echo "File ${RUST_MAKEFILE} does not exist." >&2
+fi
